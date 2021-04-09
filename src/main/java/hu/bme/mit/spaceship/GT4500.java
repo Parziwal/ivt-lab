37acfa3a46124d23e20209620500335ffa0f14d5
Stedra Kristóf <stedra99@gmail.com>
2021-04-09 13:50:03 +0200

    Delete comment from all fire method

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,10 +77,7 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
-        if (!primaryTorpedoStore.isEmpty()) {
-          firingSuccess = primaryTorpedoStore.fire(1);
-        }
+        if (!primaryTorpedoStore.isEmpty()) {firingSuccess = primaryTorpedoStore.fire(1);}
         if (!secondaryTorpedoStore.isEmpty()) {
           firingSuccess = secondaryTorpedoStore.fire(1);
         }
